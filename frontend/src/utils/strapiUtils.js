c4f59dca06204ec693f89acc4517f482c7788160
JurIVoelker <Juri.Voelker@gmx.de>
2024-07-27 18:46:38 +0200

    fix: :ambulance: missing baseurl in getStrapiImage

--- frontend/src/utils/strapiUtils.js
+++ frontend/src/utils/strapiUtils.js
@@ -1,4 +1,5 @@
 export function getStrapiImage(image) {
+  const baseUrl = process.env.NEXT_PUBLIC_STRAPI_URL || "http://127.0.0.1:1337";
   if (image?.data?.attributes?.url) {
     return `${baseUrl}${image?.data?.attributes?.url}`;
   }
