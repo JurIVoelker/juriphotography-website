98572b760bd1cf70268d6718fc0b953596bc57bb
JurIVoelker <Juri.Voelker@gmx.de>
2024-08-06 20:40:53 +0200

    test: :poop: ???

--- frontend/src/utils/apiUtils.js
+++ frontend/src/utils/apiUtils.js
@@ -3,7 +3,8 @@ import qs from "qs";
 export async function getStrapiData(path, options) {
   if (!path) throw new Error("path is undefined");
 
-  const strapiUrl = "http://127.0.0.1:1337/api";
+  const strapiUrl =
+    (process.env.NEXT_PUBLIC_STRAPI_URL || "http://127.0.0.1:1337") + "/api";
   const queryString = qs.stringify(options);
 
   const requestString = `${strapiUrl}${
